a417691a6d7018bc860dcdabb4e530e76452240e
Srujan587127 <srkallem09@gmail.com>
2026-05-12 20:17:59 -0400

    Add files via upload

---
+++ Lesson 9/Lesson 9/withoutgreenscreen.py
@@ -0,0 +1,23 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import sys
+
+
+cap = cv2.VideoCapture(0)
+bg = cv2.imread("spacebackground.jpg")
+bg = cv2.resize(bg, (640, 480))
+mp_selfie = mp.solutions.selfie_segmentation
+segment = mp_selfie.SelfieSegmentation(model_selection = 1)
+while True:
+    ret, frame = cap.read()   
+    frame = cv2.resize(frame, (640,480))
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = segment.process(rgb)
+    mask = result.segmentation_mask > 0.5
+    output = np.where(mask[:,:, None], frame, bg)
+    cv2.imshow("Virtual Background", output)
+    if cv2.waitKey(1) & 0xFF ==ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
